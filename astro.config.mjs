b87b9981b0ba28c9447cb342b51d21dd90ce0146
doubledebug <duledodicc@gmail.com>
2026-07-30 19:05:55 +0200

    added deploy workflow; bumped astro patch version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://doubledebug.github.io',
   fonts: [
     {
       provider: fontProviders.google(),
