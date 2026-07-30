6a76dec074d18a97d5995c641eb0e324d58fe35f
doubledebug <duledodicc@gmail.com>
2026-07-31 00:24:48 +0200

    minor content & CSS updates

--- src/data/content.ts
+++ src/data/content.ts
@@ -371,7 +371,7 @@ export const journey: JourneyEntry[] = [
     role: "Frontend engineer",
     org: "Ascalab",
     description:
-      "Full time frontend developer in Niš, working on multiple agile projects with teams of 3–6 people. Developed and maintained production-ready projects using NextJS, React and Typescript. Worked and communicated directly with clients, often involved in the full process of software delivery. Received feedback from senior mentors and got to work with some amazing engineers (and people!).",
+      "Full time frontend developer in Niš, working on multiple agile projects with teams of 3–6 people. Developed and maintained production-grade projects using NextJS, React and Typescript. Worked and communicated directly with clients, often involved in the full process of software delivery. Received feedback from senior mentors and got to work with some amazing engineers (and people!).",
   },
 ];
 
